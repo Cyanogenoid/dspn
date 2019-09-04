65f520b715428ce0687533115977804e8682465f
Yan Zhang <cyanogenoid@cyanogenoid.com>
2019-09-04 23:52:32 +0100

    Don't train DSPN model on RNN branch

--- dspn/scripts/all-eval-jobs.sh
+++ dspn/scripts/all-eval-jobs.sh
@@ -5,7 +5,6 @@ NUMS="1"
 
 for num in $NUMS; do
 	for dataset in $DATASETS; do
-        scripts/eval-clevr.sh $dataset $num
         scripts/eval-clevr.sh $dataset $num baseline
 	done
 done
--- dspn/scripts/all-jobs.sh
+++ dspn/scripts/all-jobs.sh
@@ -7,7 +7,6 @@ NUMS="1"  # results in paper produced with NUMS="1 2 3 4 5 6"
 
 for num in $NUMS; do
     for dataset in $DATASETS; do
-        scripts/clevr.sh $dataset $num
         scripts/clevr.sh $dataset $num baseline
     done
 done
