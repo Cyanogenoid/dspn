450c59de3e1935fc609a4d085c2f7520c41ceaed
Yan Zhang <cyanogenoid@cyanogenoid.com>
2019-09-26 07:37:29 +0100

    Fix warnings

--- dspn/utils.py
+++ dspn/utils.py
@@ -7,7 +7,7 @@ def hungarian_loss(predictions, targets, thread_pool):
     # predictions and targets shape :: (n, c, s)
     predictions, targets = outer(predictions, targets)
     # squared_error shape :: (n, s, s)
-    squared_error = F.smooth_l1_loss(predictions, targets, reduction="none").mean(1)
+    squared_error = F.smooth_l1_loss(predictions, targets.expand_as(predictions), reduction="none").mean(1)
 
     squared_error_np = squared_error.detach().cpu().numpy()
     indices = thread_pool.map(hungarian_loss_per_sample, squared_error_np)
@@ -27,7 +27,7 @@ def chamfer_loss(predictions, targets):
     # predictions and targets shape :: (k, n, c, s)
     predictions, targets = outer(predictions, targets)
     # squared_error shape :: (k, n, s, s)
-    squared_error = F.smooth_l1_loss(predictions, targets, reduction="none").mean(2)
+    squared_error = F.smooth_l1_loss(predictions, targets.expand_as(predictions), reduction="none").mean(2)
     loss = squared_error.min(2)[0] + squared_error.min(3)[0]
     return loss.view(loss.size(0), -1).mean(1)
 
